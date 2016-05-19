f3ef9ff0ad4591bcdaa86e8122750135771e256e
gholadr <david.rossellat@gmail.com>
2016-05-19 12:09:34 +0700

    adding donut

--- app/src/main/java/co/ghola/smogalert/MainActivity.java
+++ app/src/main/java/co/ghola/smogalert/MainActivity.java
@@ -1,80 +1,232 @@
 package co.ghola.smogalert;
 
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
 import co.ghola.smogalert.async.SyncUtils;
-import co.ghola.smogalert.fragment.PagerAdapter;
+import co.ghola.smogalert.db.DBContract;
+import co.ghola.smogalert.utils.Constants;
+import co.ghola.smogalert.utils.HelperSharedPreferences;
+import hugo.weaving.DebugLog;
 import io.fabric.sdk.android.Fabric;
 
 
-public class MainActivity extends AppCompatActivity {
-    private String TAG = getClass().getSimpleName();
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener,NavigationView.OnNavigationItemSelectedListener  {
+
+    private AsyncTask task = null;
+    private static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Iconify
-                .with(new FontAwesomeModule());
         Fabric.with(this, new Crashlytics());
+        Iconify.with(new FontAwesomeModule());
+        EventBus.getDefault().register(this);
         //setting up SyncService
         SyncUtils.CreateSyncAccount(this);
 
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab1));
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab2));
 
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter
-                (getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void doThis(String text){
+        if (task == null) task=new LoadCursorTask(this).execute();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        HelperSharedPreferences.putSharedPreferencesBoolean(this, HelperSharedPreferences.SharedPreferencesKeys.notificationKey,isChecked);
+
+    }
+
+    abstract private class BaseTask<T> extends AsyncTask<T, Void, Cursor> {
+        final ContentResolver resolver;
+
+        BaseTask(Context ctxt) {
+            super();
+
+            resolver=ctxt.getContentResolver();
+        }
+
+        @Override
+        public void onPostExecute(Cursor result) {
+            if (result.getCount() > 0) {
+                result.moveToPosition(0);
+                String time = new DateTime((result.getLong(DBContract.COLUMN_IDX_TS) * 1000), DateTimeZone.UTC).toString("MMM d  haa");
+                String aqi = result.getString(DBContract.COLUMN_IDX_AQI);
+                String msg = result.getString(DBContract.COLUMN_IDX_MESSAGE);
+                String blurb = "";
+                TextView view = (TextView) findViewById(R.id.aqi);
+                view.setText(aqi + " " + getResources().getString(R.string.aqi_text));
+                view = (TextView) findViewById(R.id.message);
+                view.setText(msg);
+                view = (TextView) findViewById(R.id.date);
+                view.setText(time);
+                Integer previousLevel = HelperSharedPreferences.getSharedPreferencesInt(getApplicationContext(), HelperSharedPreferences.SharedPreferencesKeys.levelsKey,-1);
+
+                switch (previousLevel){
+
+                    case Constants.GOOD : blurb = getApplicationContext().getResources().getString(R.string.good_blurb);
+                        break;
+                    case Constants.MODERATE : blurb = getApplicationContext().getResources().getString(R.string.moderate_blurb);
+                        break;
+                    case Constants.SENSITIVE : blurb = getApplicationContext().getResources().getString(R.string.sensitive_blurb);
+                        break;
+                    case Constants.UNHEALTHY : blurb = getApplicationContext().getResources().getString(R.string.unhealthy_blurb);
+                        break;
+                }
+                view = (TextView) findViewById(R.id.blurb);
+                view.setText(blurb);
+
+                GaugeView gaugeView = (GaugeView) findViewById(R.id.gauge);
+                gaugeView.setGaugeValue(aqi);
+                gaugeView.setVisibility(View.VISIBLE);
             }
+            task=null;
+        }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
+        @DebugLog
+        protected Cursor doQuery() {
+            Cursor result=resolver.query(DBContract.AirQualitySample.CONTENT_URI,
+                    DBContract.PROJECTION, null, null, "ts DESC LIMIT 1");
 
-            }
+            return(result);
+        }
+    }
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
+    private class LoadCursorTask extends BaseTask<Void> {
+        LoadCursorTask(Context ctxt) {
+            super(ctxt);
+        }
 
-            }
-        });
+        @Override
+        protected Cursor doInBackground(Void... params) {
+            return (doQuery());
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+       // getMenuInflater().inflate(R.menu.main, menu);
+        SwitchCompat switchCompat = (SwitchCompat) findViewById(R.id.switch_compat);
+        Boolean switchOn = HelperSharedPreferences.getSharedPreferencesBoolean(getApplicationContext(),HelperSharedPreferences.SharedPreferencesKeys.notificationKey, false);
+        switchCompat.setChecked(switchOn);
+        switchCompat.setOnCheckedChangeListener(this);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onDestroy(){
+
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+
+    }
+
+    @Override
+    public void onResume(){
+
+        super.onResume();
+        if (task==null) task=new LoadCursorTask(this).execute();
+    }
+
+    public static Bitmap scaleDown(Bitmap realImage, float maxImageSize,
+                                   boolean filter) {
+        float ratio = Math.min(
+                (float) maxImageSize / realImage.getWidth(),
+                (float) maxImageSize / realImage.getHeight());
+        int width = Math.round((float) ratio * realImage.getWidth());
+        int height = Math.round((float) ratio * realImage.getHeight());
+
+        Bitmap newBitmap = Bitmap.createScaledBitmap(realImage, width,
+                height, filter);
+        return newBitmap;
+    }
 
 }
---
+++ app/src/main/java/co/ghola/smogalert/utils/Constants.java
@@ -0,0 +1,12 @@
+package co.ghola.smogalert.utils;
+
+/**
+ * Created by gholadr on 5/18/16.
+ */
+public class Constants {
+
+    public static final int GOOD = 49;
+    public static final int MODERATE = 99;
+    public static final int SENSITIVE = 149;
+    public static final int UNHEALTHY = 199;
+}
