252f796fd5b1a8812a537070a4858ada289668c5
Alecks Johansson <alecksjohansson@Aleckss-MacBook-Pro.local>
2016-08-03 10:36:54 +0700

    Change NavBar Color and Fix null errors

--- app/src/main/java/co/ghola/smogalert/fragments/SummaryFragment.java
+++ app/src/main/java/co/ghola/smogalert/fragments/SummaryFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -78,34 +79,29 @@ public class SummaryFragment extends Fragment {
                 Calendar calendar = Calendar.getInstance();
                 String am_pm;
                 int hours = calendar.get( Calendar.HOUR );
-                if(Integer.parseInt(shareText) > 150)
-                {
-                    Glide.with(getContext()).load(R.drawable.ninja1).centerCrop().into(imageView);
-                }
-                if(Integer.parseInt(shareText) < 149)
-                {
-                    if( calendar.get( Calendar.AM_PM ) == 0 ){
-                        am_pm = "AM";
-                        if(hours >= 6 && am_pm.equals("AM")) {
-                            Glide.with(getContext()).load(R.drawable.bg1).centerCrop().into(imageView);
-                        }
-                        else
-                        {
-                            Glide.with(getContext()).load(R.drawable.sandiegonight).centerCrop().into(imageView);
-                        }
-
+                if(shareText != null) {
+                    if (Integer.parseInt(shareText) > 150) {
+                        Glide.with(getContext()).load(R.drawable.ninja1).centerCrop().into(imageView);
                     }
-                    else{
-                        am_pm = "PM";
-                        if(hours >= 6 && am_pm.equals("PM")) {
-                            Glide.with(getContext()).load(R.drawable.sandiegonight).centerCrop().into(imageView);
-                            System.out.println("welcome");
-                        }
-                        else
-                        {
-                            Glide.with(getContext()).load(R.drawable.bg1).centerCrop().into(imageView);
-                        }
+                    if (Integer.parseInt(shareText) < 149) {
+                        if (calendar.get(Calendar.AM_PM) == 0) {
+                            am_pm = "AM";
+                            if (hours >= 6 && am_pm.equals("AM")) {
+                                Glide.with(getContext()).load(R.drawable.bg1).centerCrop().into(imageView);
+                            } else {
+                                Glide.with(getContext()).load(R.drawable.sandiegonight).centerCrop().into(imageView);
+                            }
 
+                        } else {
+                            am_pm = "PM";
+                            if (hours >= 6 && am_pm.equals("PM")) {
+                                Glide.with(getContext()).load(R.drawable.sandiegonight).centerCrop().into(imageView);
+                                System.out.println("welcome");
+                            } else {
+                                Glide.with(getContext()).load(R.drawable.bg1).centerCrop().into(imageView);
+                            }
+
+                        }
                     }
                 }
 
