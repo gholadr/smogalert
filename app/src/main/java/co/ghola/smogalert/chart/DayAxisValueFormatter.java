166cb10f570f6f139f2421939f498a488a3e8aad
Alecks Johansson <alecksjohansson@Aleckss-MacBook-Pro.local>
2016-08-05 15:49:28 +0700

    Last 24 Hours Graph Fixed

--- app/src/main/java/co/ghola/smogalert/chart/DayAxisValueFormatter.java
+++ app/src/main/java/co/ghola/smogalert/chart/DayAxisValueFormatter.java
@@ -28,16 +28,8 @@ public class DayAxisValueFormatter implements AxisValueFormatter {
 
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
-        if((int) value == 7){
-
-            return "";
-        }
-        int days = (int) value * 4;
-        if(days<12){
-            return days+" AM";
-        }else {
-            return (days-12)+" PM";
-        }
+      int days = (int) value;
+        return days+ "";
 
     }
 
